dbda9f2f02b79e4b01cc83c48855d75191f5f596
Jonathan Choi <jonchoi11@gmail.com>
2021-11-02 12:00:06 -0600

    fixing state

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,28 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function Shop() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch(
+      "https://fortnite-api.theapinetwork.com/upcoming/get"
+    );
+
+    const items = await data.json();
+    console.log(items.data);
+    setItems(items.data);
+  };
+
   return (
     <div>
-      <h1>Shop Page</h1>
+      {items.map((item) => (
+        <h1 key={item.dataid}>{item.data}</h1>
+      ))}
     </div>
   );
 }
